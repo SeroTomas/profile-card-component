93fdb9c44a7061838ca7afca108c0e6b670198a6
SeroTomas <serocontacto@gmail.com>
2024-01-08 20:47:07 -0300

    working on profile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"c-darkCyan": "hsl(185, 75%, 39%)",
+				"c-darkDesaturedBlue":"hsl(229, 23%, 23%)",
+				"c-grayishBlue": "hsl(227, 10%, 46%)",
+				"c-darkGray": "hsl(0, 0%, 59%)"
+			},
+			fontFamily:{
+				"c-kumbh": ['Kumbh Sans', 'sans-serif']
+			},
+			backgroundImage:{
+				"c-cardBg": "url('images/bg-pattern-card.svg')"
+			}
+			
+		},
 	},
 	plugins: [],
 }
